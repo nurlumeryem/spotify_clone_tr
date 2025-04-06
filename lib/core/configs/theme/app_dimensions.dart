d3e09866e2e600e5a632a2cd45dc8385f9449a16
MERYEM NURLU <128179240+nurlumeryem@users.noreply.github.com>
2025-04-07 00:12:11 +0300

    Centralize width and height constants in app_dimensions.dart for better project structure and consistency

---
+++ lib/core/configs/theme/app_dimensions.dart
@@ -0,0 +1,4 @@
+class AppDimensions {
+  static const double figmaDesignWidth = 390.0; // Figma design width
+  static const double figmaDesignHeight = 844.0; // Figma design height
+}
--- lib/presentation/pages/splash.dart
+++ lib/presentation/pages/splash.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
-// Kendi projenizdeki doğru yolu kullandığınızdan emin olun
+import 'package:spotify_clone_tr/core/configs/theme/app_dimensions.dart';
+
 import 'package:spotify_clone_tr/core/configs/theme/app_images.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
-  static const double figmaTasarimGenisligi = 390.0;
   @override
   Widget build(BuildContext context) {
-    final double ekranGenisligi = MediaQuery.of(context).size.width;
-    final double responsiveGenislik =
-        (196 / figmaTasarimGenisligi) * ekranGenisligi;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    final double responsiveWidth =
+        (196 / AppDimensions.figmaDesignWidth) * screenWidth;
     return Scaffold(
       body: Center(
         child: Image.asset(
           AppImages.spotifyIcon,
-          width: responsiveGenislik,
+          width: responsiveWidth,
           fit: BoxFit.contain,
         ),
       ),
