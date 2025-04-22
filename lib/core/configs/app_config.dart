65a927427b945eec53d29dc84001c79f88341b16
MERYEM NURLU <128179240+nurlumeryem@users.noreply.github.com>
2025-04-22 13:12:07 +0300

    feat: add AuthFirebaseService implementation for authentication logic

--- lib/core/configs/app_config.dart
+++ lib/core/configs/app_config.dart
@@ -1,8 +1,20 @@
 import 'package:get_it/get_it.dart';
 import 'package:spotify_clone_tr/core/configs/router/app_router.dart';
+import 'package:spotify_clone_tr/data/repositories/auth_repository_impl.dart';
+import 'package:spotify_clone_tr/domain/repositories/auth_repository.dart';
+import 'package:spotify_clone_tr/domain/usecases/signup_usecase.dart';
 
 final GetIt getIt = GetIt.instance;
 
 void setupLocator() {
+  // Router
   getIt.registerSingleton<AppRouter>(AppRouter());
+
+  // Repository
+  getIt.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
+
+  // UseCase
+  getIt.registerLazySingleton<SignupUseCase>(
+    () => SignupUseCase(getIt<AuthRepository>()),
+  );
 }
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,14 +1,19 @@
 import 'package:get_it/get_it.dart';
 import 'package:spotify_clone_tr/data/repositories/auth_repository_impl.dart';
+import 'package:spotify_clone_tr/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify_clone_tr/domain/repositories/auth_repository.dart';
+import 'package:spotify_clone_tr/domain/usecases/signin_usecase.dart';
 import 'package:spotify_clone_tr/domain/usecases/signup_usecase.dart';
 
 final sl = GetIt.instance;
+Future<void> initializeDependencies() async {
+  // AuthFirebaseService
+  sl.registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
 
-void setupServiceLocator() {
-  // Repositories
-  sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
+  sl.registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
 
-  // Use cases
-  sl.registerLazySingleton(() => SignupUseCase(sl<AuthRepository>()));
+  // UseCases -> Bağımlılığı: AuthRepository
+  sl.registerSingleton<SignupUseCase>(SignupUseCase(sl<AuthRepository>()));
+
+  sl.registerSingleton<SigninUseCase>(SigninUseCase(sl<AuthRepository>()));
 }
