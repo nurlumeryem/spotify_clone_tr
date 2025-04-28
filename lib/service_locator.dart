6406c3bec8f6f63da8163ec62fd698cb6bc60cef
MERYEM NURLU <128179240+nurlumeryem@users.noreply.github.com>
2025-04-28 23:04:08 +0300

    feat: Add Supabase music service dependency injection

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -5,6 +5,7 @@ import 'package:spotify_clone_tr/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify_clone_tr/domain/repositories/auth_repository.dart';
 import 'package:spotify_clone_tr/domain/usecases/signin_usecase.dart';
 import 'package:spotify_clone_tr/domain/usecases/signup_usecase.dart';
+import 'package:spotify_clone_tr/service/music_service.dart';
 
 final sl = GetIt.instance;
 Future<void> initializeDependencies() async {
@@ -19,4 +20,7 @@ Future<void> initializeDependencies() async {
   // UseCases -> Bağımlılığı: AuthRepository
   sl.registerSingleton<SignupUseCase>(SignupUseCase(sl<AuthRepository>()));
   sl.registerSingleton<SigninUseCase>(SigninUseCase());
+
+  // SupabaseMusicService'i
+  sl.registerSingleton<SupabaseMusicService>(SupabaseMusicService());
 }
