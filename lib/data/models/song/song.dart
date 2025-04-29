79e7e52d4a06f324b0bb565a282d4059430c3e26
MERYEM NURLU <128179240+nurlumeryem@users.noreply.github.com>
2025-04-29 14:31:56 +0300

    update song model

--- lib/data/models/song/song.dart
+++ lib/data/models/song/song.dart
@@ -2,10 +2,30 @@ class Song {
   final int id;
   final String title;
   final String artist;
+  final String filePath;
+  final String url;
+  final String userId;
+  final DateTime createdAt;
 
-  Song({required this.id, required this.title, required this.artist});
+  Song({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.filePath,
+    required this.url,
+    required this.userId,
+    required this.createdAt,
+  });
 
   factory Song.fromJson(Map<String, dynamic> json) {
-    return Song(id: json['id'], title: json['title'], artist: json['artist']);
+    return Song(
+      id: json['id'],
+      title: json['title'],
+      artist: json['artist'],
+      filePath: json['file_path'],
+      url: json['url'],
+      userId: json['user_id'],
+      createdAt: DateTime.parse(json['created_at']),
+    );
   }
 }
