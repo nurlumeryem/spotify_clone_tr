e675e88817d92dcd8399f1a038e67a79deb252da
MERYEM NURLU <128179240+nurlumeryem@users.noreply.github.com>
2025-05-01 14:55:32 +0300

    feat: add GetNewsUsecase for fetching latest songs  - Implemented GetNewsUsecase to encapsulate the logic for retrieving the latest songs. - Uses service locator to access SongRepositoryImpl. - Provides a clean use case layer for the news songs feature.

--- lib/data/repositories/songs_repository_impl.dart
+++ lib/data/repositories/songs_repository_impl.dart
@@ -1,5 +1,5 @@
 import 'package:spotify_clone_tr/core/utils/result.dart';
-import 'package:spotify_clone_tr/data/sources/song/song.dart';
+import 'package:spotify_clone_tr/data/sources/song/song_supabase_service.dart';
 import 'package:spotify_clone_tr/domain/repositories/song_repository.dart';
 import 'package:spotify_clone_tr/service_locator.dart';
 
---
+++ lib/domain/usecases/get_news_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:spotify_clone_tr/core/usecase/usecase.dart';
+import 'package:spotify_clone_tr/core/utils/result.dart';
+import 'package:spotify_clone_tr/data/repositories/songs_repository_impl.dart';
+
+import 'package:spotify_clone_tr/service_locator.dart';
+
+class GetNewsUsecase implements UseCase<Result, dynamic> {
+  @override
+  Future<Result<void>> call({params}) async {
+    return await sl<SongRepositoryImpl>().getNewsSongs();
+  }
+}
