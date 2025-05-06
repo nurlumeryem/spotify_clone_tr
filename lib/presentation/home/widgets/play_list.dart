bcce983a213ebccab3a72ca40689699120292104
MERYEM NURLU <128179240+nurlumeryem@users.noreply.github.com>
2025-05-07 00:01:57 +0300

    create widget playlist

---
+++ lib/presentation/home/widgets/play_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlayList extends StatelessWidget {
+  const PlayList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
